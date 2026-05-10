f24a904d30fab3f5e0028920f566823b7771e8f9
Martin Kihahu <martin.kihahu@gmail.com>
2026-05-10 12:44:23 +0300

    Login and register

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
+app.use("/api/user", userRouter);
+
 app.get("/", (req, res) => {
   res.send("API Working");
 });
